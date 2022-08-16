e851e790abbf784a3332bbd7c37aa4d9af74298b
BlaiseBaptist <40903991+BlaiseBaptist@users.noreply.github.com>
2022-08-16 19:48:11 -0400

    added the rest of the basic models

--- bunknotes/admin.py
+++ bunknotes/admin.py
@@ -1,5 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Family
+from .models import Family,Sender,Cabin,Camper,Letter,tx#,Photo
 admin.site.register(Family)
+admin.site.register(Sender)
+admin.site.register(Cabin)
+admin.site.register(Camper)
+admin.site.register(Letter)
+admin.site.register(tx)
+#admin.site.register(Photo)
+
+
+
--- bunknotes/models.py
+++ bunknotes/models.py
@@ -8,4 +8,48 @@ class Family(models.Model):
     def __str__(self):
         return self.name + "( $" + str(self.balance) + ")"
 
+class Sender(models.Model):
+    family = models.ForeignKey(Family,on_delete=models.CASCADE)
+    firstName = models.CharField(max_length=100)
+    lastName = models.CharField(max_length=100)
+    username = models.CharField(max_length=100)
+    password = models.CharField(max_length=100)
+    def __str__(self):
+        return self.firstName + " " + self.lastName +" ("+self.family.name+") "
 
+class Cabin(models.Model):
+    name = models.CharField(max_length=100)
+    def __str__(self):
+        return self.name
+
+class Camper(models.Model):
+    family = models.ForeignKey(Family,on_delete=models.CASCADE)
+    cabin = models.ForeignKey(Cabin,on_delete=models.CASCADE)
+    firstName = models.CharField(max_length=100)
+    lastName = models.CharField(max_length=100)
+    def __str__(self):
+        return self.firstName +" "+ self.lastName +" ("+self.family.name+") "+self.cabin.name
+
+class tx(models.Model):
+    date = models.DateField()
+    sender = models.ForeignKey(Sender,on_delete=models.CASCADE)
+    family = models.ForeignKey(Family,on_delete=models.CASCADE)
+    amount = models.DecimalField(decimal_places=2,max_digits=10)
+
+
+class Letter(models.Model):
+    sender = models.ForeignKey(Sender,on_delete=models.CASCADE)
+    camper = models.ForeignKey(Camper,on_delete=models.CASCADE)
+    tx = models.ForeignKey(tx,on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    body = models.TextField()
+    isDelivered = models.BooleanField()
+    date = models.DateField()
+    def __str__(self):
+        return self.title + " " +str(self.sender) + " "+str(self.camper)+ " "+self.date
+
+class Photo():
+   letter = models.ForeignKey(Letter,on_delete=models.CASCADE)
+   data = models.URLField()
+# TODO
+#enrcypt passwords
