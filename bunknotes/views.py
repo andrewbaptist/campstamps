e20fb94aa369472ad8be5add0913f7e46e0979e3
BlaiseBaptist <40903991+BlaiseBaptist@users.noreply.github.com>
2022-08-16 20:35:17 -0400

    added a page

--- bunknotes/views.py
+++ bunknotes/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
 
 # Create your views here.
-
+from .models import Family
+from django.template import loader
 from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+def families(request):
+    family_list = Family.objects.order_by('-balance')
+    template = loader.get_template('bunknotes/families.html')
+    context ={
+            'family_list':family_list
+            }
+    return HttpResponse(template.render(context,request))
